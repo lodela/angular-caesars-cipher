3fc1b138320f5a0171d955c2341f6e3f4b0f555a
lodela <lodela.n@gmail.com>
2020-09-21 19:34:38 -0500

    6 files modified, 16 added, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'cipher-app',
+  template: `
+            <shift></shift>
+            <div class="inputArea">
+              <plaintext></plaintext>
+              <ciphertext></ciphertext>
+            </div>
+            `
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+
+import { CiphertextComponent } from './ciphertext/ciphertext.component';
+import { ShiftComponent } from './shift/shift.component';
+import { ShiftValueService } from './shiftvalue.service';
+import { PlaintextComponent } from './plaintext/plaintext.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    PlaintextComponent,
+    CiphertextComponent,
+    ShiftComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [ShiftValueService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
